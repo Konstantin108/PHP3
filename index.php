a164bf5051a6adec7396c9cd879571343cd19e56
Kostya <Kpstya@mail.ru>
2020-11-01 23:50:52 +0300

    hw3

--- index.php
+++ index.php
@@ -1,60 +1,22 @@
+<h1 style="color: red">Четвёртое задание</h1>
 <?php
 
-class SquareSize
+ $arr = [12, 5, 6, 1, 11, 10, 8, 9, 4, 3, 2, 7];
+
+function mergeSort($myArr)
 {
-	public $height = '';
-	public $width = '';
-	public $arr = [];
+    echo '<pre>';
+    $newArr = array_chunk($myArr, 2);
 
-	public function multiply()
-	{
-		return $this->height * $this->width;
-	}
+    print_r($newArr);
 
-	public function getArray()
-	{
-		$n =  $this->multiply();
+    foreach($newArr as  $value){
+        if(is_array($value)){
 
-		for($i = 1; $i <= $n; $i++){
-			$this->arr[] = $i + 1;
-		}
+              echo  array_keys ($value , '0');
 
-	}
-
-	public function getHeight()
-	{
-			$h = $this->height;
-			$array = $this->arr;
-			for($k = 0; $k < count($array); $k++){
-				for($j = 1; $j <= $h; $j++){
-				echo $j;
-			}
-		}
-
-	}
+        }
+    }
 }
 
-$squareSize = new SquareSize();
-$squareSize->height = 5;
-$squareSize->width = 3;
-$squareSize->multiply();
-$squareSize->getArray();
-$squareSize->getHeight();
-
-// class GetArray extends SquareSize
-// {
-// 	public $lengthOfArr;
-
-// 	public function __construct($height, $width, $resultOfMultiply, $lengthOfArr)
-// 	{
-// 		$this->lengthOfArr = $lengthOfArr;
-// 		parent::__construct($height, $width, $resultOfMultiply);
-// 	}
-
-
-// 	public function display()
-// 	{
-// 		echo $this->lengthOfArr;
-// 	}
-// }
-
+mergeSort($arr);
